08bf01721273fd24a4cd74893007c1baf61af9ef
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-08 12:39:24 +0530

    Adding PUT Endpoint
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- task/views.py
+++ task/views.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
-from djcelery.admin_utils import action
 from rest_framework import routers, serializers, viewsets
-from rest_framework.response import Response
 
 from task.models import TaskTracker, Task
 
@@ -50,23 +48,6 @@ class TaskViewSet(viewsets.ModelViewSet):
     """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-    @action(detail=True,
-            methods=['put'],
-            name='Update Task',
-            short_description="Update the Task View")
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.task_type = request.data.get("task_type")
-        instance.update_type = request.data.get("update_type")
-        instance.email = request.data.get("email")
-        instance.save()
-
-        serializer = self.get_serializer(instance)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        return Response(serializer.data)
 
 
 class TaskTrackerViewSet(viewsets.ModelViewSet):
